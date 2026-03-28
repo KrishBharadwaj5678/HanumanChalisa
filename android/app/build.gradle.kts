3f28d93d53b63562018819b1efa22fd05505b010
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2026-03-28 09:05:08 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.krishlabs.hanumanchalisa"
+    namespace = "com.byteaura.hanumanchalisa"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.krishlabs.hanumanchalisa"
+        applicationId = "com.byteaura.hanumanchalisa"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
